cf8b7d8f0474b56ffdbd1f6a150adccfc4cb5c71
Freed GB <gfreednel@gmail.com>
2025-07-15 15:14:05 +0100

    Implement event adding functionality

--- main.cpp
+++ main.cpp
@@ -53,11 +53,12 @@ class Event
 
 
 // Custom Functions
-Event LorentzTransform(Event point, float velocity);
+Event LorentzTransform(Event point, float observerVelocity);
 float WorldToScreenX(float worldX, int offsetX);
 float WorldToScreenY(float worldY, int offsetY);
 float ScreenToWorldX(float screenX, int offsetX);
 float ScreenToWorldY(float screenY, int offsetY);
+bool MouseInGrid(Vector2 mouse, float gridWidth, float gridHeight);
 
 int main()
 {
@@ -69,28 +70,50 @@ int main()
     int globeHeight;
     float panelWidth;
     float panelHeight;
-    float velocity;
+    float observerVelocity;
     float currentX;
     float currentY;
     std::vector<Event> eventsList;
     bool draggingMode;
+    bool addingMode;
+    int colorIndex;
     Vector2 mousePosition;
     int eventDraggedIndex;
+    int activeToggle = -1;
 
     // Define and initialize generic limit points
+    Event e_lab_0 = Event(0.0, 0.0, BLACK);
+    Event e_lab_1 = Event(0.0, 0.0, BLACK);
+    Event e_lab_2 = Event(0.0, 0.0, BLACK);
+    Event e_lab_3 = Event(0.0, 0.0, BLACK);
     Event e0 = Event(0.0, 0.0, BLACK);
     Event e1 = Event(0.0, 0.0, BLACK);
-    Event p1 = Event(0.0, 0.0, BLACK);
-    Event p2 = Event(0.0, 0.0, BLACK);
-    Event p3 = Event(0.0, 0.0, BLACK);
-    Event p4 = Event(0.0, 0.0, BLACK);
-    Event e0Prime = Event(0.0, 0.0, BLACK);
-    Event e1Prime = Event(0.0, 0.0, BLACK);
-    Event p1Prime = Event(0.0, 0.0, BLACK);
-    Event p2Prime = Event(0.0, 0.0, BLACK);
-    Event p3Prime = Event(0.0, 0.0, BLACK);
-    Event p4Prime = Event(0.0, 0.0, BLACK);
+    Event e2 = Event(0.0, 0.0, BLACK);
+    Event e3 = Event(0.0, 0.0, BLACK);
 
+    Event p_lab_1 = Event(0.0, 0.0, BLACK);
+    Event p_lab_2 = Event(0.0, 0.0, BLACK);
+    Event p_lab_3 = Event(0.0, 0.0, BLACK);
+    Event p_lab_4 = Event(0.0, 0.0, BLACK);
+    Event p_obs_1 = Event(0.0, 0.0, BLACK);
+    Event p_obs_2 = Event(0.0, 0.0, BLACK);
+    Event p_obs_3 = Event(0.0, 0.0, BLACK);
+    Event p_obs_4 = Event(0.0, 0.0, BLACK);
+
+    // Colors
+    const Color MAIN_RED = RED;
+    const Color SECONDARY_RED = (Color){0xe6, 0x29, 0x37, 0xb8};
+    const Color MAIN_BLUE = BLUE;
+    const Color SECONDARY_BLUE = (Color){0x00, 0x79, 0xf1, 0x80};
+    const Color MAIN_GREEN = GREEN;
+    const Color SECONDARY_GREEN = (Color){0x00, 0xe4, 0x30, 0x80};
+
+    // Define event color list. Each element is a couple (color in adding mode, main color)
+    std::vector<std::vector<Color>> eventColorList = {
+        {MAIN_RED, SECONDARY_RED},
+        {MAIN_BLUE, SECONDARY_BLUE},
+        {MAIN_GREEN, SECONDARY_GREEN}
+    };
 
     // Game loop
     while (!WindowShouldClose())
@@ -143,12 +166,17 @@ int main()
 
             
             // --- Spacetime Globe ---
+            /*
+                The main grid (the one always fixed) represents the frame of reference of the observer moving at v m/s relative to the lab frame.
+                The secondary grid (the one which deforms) represents the lab frame viewed in the frame of the moving observer.
+            */
 
             // Draw labels
             DrawText("space", MARGIN + globeWidth - 70, MARGIN + globeHeight/2 + 10, 20, TEXT_COLOR);
             DrawText("time", MARGIN + globeWidth/2 + 10, MARGIN + 10, 20, TEXT_COLOR);
             
-            // Draw Axes
+            
+            // Draw OBSERVER FRAME's axes
             DrawLineEx(
                 (Vector2){MARGIN + globeWidth/2, MARGIN},
                 (Vector2){MARGIN + globeWidth/2, MARGIN + globeHeight},
@@ -162,110 +190,124 @@ int main()
                 GRID_COLOR
             );
 
-            // Grid
+            // Draw LAB FRAME's axes on the OBSERVER's FRAME
+
+            // Axe's points coordinates in the lab's frame
             e0 = Event(0.0, ScreenToWorldY(MARGIN, globeHeight/2), BLACK);
             e1 = Event(0.0, ScreenToWorldY(MARGIN + globeHeight, globeHeight/2), BLACK);
-            e0Prime = LorentzTransform(e0, velocity);
-            e1Prime = LorentzTransform(e1, velocity);
+            e2 = Event(ScreenToWorldX(MARGIN, globeWidth/2), 0.0, BLACK);
+            e3 = Event(ScreenToWorldX(MARGIN + globeWidth, globeWidth/2), 0.0, BLACK);
+
+            // Those axe's points coordinates in the observer's frame (the lab moves at -observerVelocity)
+            e_lab_0 = LorentzTransform(e0, observerVelocity);
+            e_lab_1 = LorentzTransform(e1, observerVelocity);
+            e_lab_2 = LorentzTransform(e2, observerVelocity);
+            e_lab_3 = LorentzTransform(e3, observerVelocity);
+
+            // Drawing axes
             DrawLineEx(
-                (Vector2){WorldToScreenX(e0Prime.getX(), globeWidth/2), WorldToScreenY(e0Prime.getT(), globeHeight/2)},
-                (Vector2){WorldToScreenX(e1Prime.getX(), globeWidth/2), WorldToScreenY(e1Prime.getT(), globeHeight/2)},
+                (Vector2){WorldToScreenX(e_lab_0.getX(), globeWidth/2), WorldToScreenY(e_lab_0.getT(), globeHeight/2)},
+                (Vector2){WorldToScreenX(e_lab_1.getX(), globeWidth/2), WorldToScreenY(e_lab_1.getT(), globeHeight/2)},
                 SECONDARY_GRID_THICKNESS,
                 SECONDARY_GRID_COLOR
             );
+            DrawLineEx(
+                (Vector2){WorldToScreenX(e_lab_2.getX(), globeWidth/2), WorldToScreenY(e_lab_2.getT(), globeHeight/2)},
+                (Vector2){WorldToScreenX(e_lab_3.getX(), globeWidth/2), WorldToScreenY(e_lab_3.getT(), globeHeight/2)},
+                SECONDARY_GRID_THICKNESS,
+                SECONDARY_GRID_COLOR
+            );
+
+
+            // Drawing frames' vertical lines
             int s = globeWidth / GRID_SPACING;
             for (int i = 1; i <= s/2; i++)
-            {
+            {   
+                // Vertical lines of the OBSERVER's frame (they coincide with LAB's frame at v = 0)
+                p_obs_1 = Event(-i, ScreenToWorldY(MARGIN, globeHeight/2), BLACK);
+                p_obs_2 = Event(-i, ScreenToWorldY(MARGIN + globeHeight, globeHeight/2), BLACK);
+                p_obs_3 = Event(i, ScreenToWorldY(MARGIN, globeHeight/2), BLACK);
+                p_obs_4 = Event(i, ScreenToWorldY(MARGIN + globeHeight, globeHeight/2), BLACK);
                 
-                p1 = Event(-i, ScreenToWorldY(MARGIN, globeHeight/2), BLACK);
-                p2 = Event(-i, ScreenToWorldY(MARGIN + globeHeight, globeHeight/2), BLACK);
-                p3 = Event(i, ScreenToWorldY(MARGIN, globeHeight/2), BLACK);
-                p4 = Event(i, ScreenToWorldY(MARGIN + globeHeight, globeHeight/2), BLACK);
-                
-                p1Prime = LorentzTransform(p1, velocity);
-                p2Prime = LorentzTransform(p2, velocity);
-                p3Prime = LorentzTransform(p3, velocity);
-                p4Prime = LorentzTransform(p4, velocity);
+                // Vertical lines of the LAB's frame viewed in the OBSERVER's frame
+                p_lab_1 = LorentzTransform(p_obs_1, observerVelocity);
+                p_lab_2 = LorentzTransform(p_obs_2, observerVelocity);
+                p_lab_3 = LorentzTransform(p_obs_3, observerVelocity);
+                p_lab_4 = LorentzTransform(p_obs_4, observerVelocity);
 
-                // Draw background grid (vertical)
+                // Drawing LAB's frame vertical lines
                 DrawLineEx(
-                    (Vector2){WorldToScreenX(p1Prime.getX(), globeWidth/2), WorldToScreenY(p1Prime.getT(), globeHeight/2)},
-                    (Vector2){WorldToScreenX(p2Prime.getX(), globeWidth/2), WorldToScreenY(p2Prime.getT(), globeHeight/2)},
+                    (Vector2){WorldToScreenX(p_lab_1.getX(), globeWidth/2), WorldToScreenY(p_lab_1.getT(), globeHeight/2)},
+                    (Vector2){WorldToScreenX(p_lab_2.getX(), globeWidth/2), WorldToScreenY(p_lab_2.getT(), globeHeight/2)},
                     SECONDARY_GRID_THICKNESS,
                     SECONDARY_GRID_COLOR
                 );
                 DrawLineEx(
-                    (Vector2){WorldToScreenX(p3Prime.getX(), globeWidth/2), WorldToScreenY(p3Prime.getT(), globeHeight/2)},
-                    (Vector2){WorldToScreenX(p4Prime.getX(), globeWidth/2), WorldToScreenY(p4Prime.getT(), globeHeight/2)},
+                    (Vector2){WorldToScreenX(p_lab_3.getX(), globeWidth/2), WorldToScreenY(p_lab_3.getT(), globeHeight/2)},
+                    (Vector2){WorldToScreenX(p_lab_4.getX(), globeWidth/2), WorldToScreenY(p_lab_4.getT(), globeHeight/2)},
                     SECONDARY_GRID_THICKNESS,
                     SECONDARY_GRID_COLOR
                 );
 
 
-                // Draw main grid (vertical)
+                // Drawing OBSERVER's frame vertical lines
                 DrawLineEx(
-                    (Vector2){WorldToScreenX(p1.getX(), globeWidth/2), WorldToScreenY(p1.getT(), globeHeight/2)},
-                    (Vector2){WorldToScreenX(p2.getX(), globeWidth/2), WorldToScreenY(p2.getT(), globeHeight/2)},
+                    (Vector2){WorldToScreenX(p_obs_1.getX(), globeWidth/2), WorldToScreenY(p_obs_1.getT(), globeHeight/2)},
+                    (Vector2){WorldToScreenX(p_obs_2.getX(), globeWidth/2), WorldToScreenY(p_obs_2.getT(), globeHeight/2)},
                     GRID_THICKNESS,
                     GRID_COLOR
                 );
                 DrawLineEx(
-                    (Vector2){WorldToScreenX(p3.getX(), globeWidth/2), WorldToScreenY(p3.getT(), globeHeight/2)},
-                    (Vector2){WorldToScreenX(p4.getX(), globeWidth/2), WorldToScreenY(p4.getT(), globeHeight/2)},
+                    (Vector2){WorldToScreenX(p_obs_3.getX(), globeWidth/2), WorldToScreenY(p_obs_3.getT(), globeHeight/2)},
+                    (Vector2){WorldToScreenX(p_obs_4.getX(), globeWidth/2), WorldToScreenY(p_obs_4.getT(), globeHeight/2)},
                     GRID_THICKNESS,
                     GRID_COLOR
                 );
 
             }
 
-            e0 = Event(ScreenToWorldX(MARGIN, globeWidth/2), 0.0, BLACK);
-            e1 = Event(ScreenToWorldX(MARGIN + globeWidth, globeWidth/2), 0.0, BLACK);
-            e0Prime = LorentzTransform(e0, velocity);
-            e1Prime = LorentzTransform(e1, velocity);
-            DrawLineEx(
-                (Vector2){WorldToScreenX(e0Prime.getX(), globeWidth/2), WorldToScreenY(e0Prime.getT(), globeHeight/2)},
-                (Vector2){WorldToScreenX(e1Prime.getX(), globeWidth/2), WorldToScreenY(e1Prime.getT(), globeHeight/2)},
-                SECONDARY_GRID_THICKNESS,
-                SECONDARY_GRID_COLOR
-            );
+            
+            // Drawing frames' horizontal lines
             int t = globeHeight / GRID_SPACING;
             for (int i = 1; i <= t/2; i++)
             {
-                p1 = Event(ScreenToWorldX(MARGIN, globeWidth/2), -i, BLACK);
-                p2 = Event(ScreenToWorldX(MARGIN + globeWidth, globeWidth/2), -i, BLACK);
-                p3 = Event(ScreenToWorldX(MARGIN, globeWidth/2), i, BLACK);
-                p4 = Event(ScreenToWorldX(MARGIN + globeWidth, globeWidth/2), i, BLACK);
+                // Horizontal lines of the OBSERVER's frame (they coincide with LAB's frame at v = 0)
+                p_obs_1 = Event(ScreenToWorldX(MARGIN, globeWidth/2), -i, BLACK);
+                p_obs_2 = Event(ScreenToWorldX(MARGIN + globeWidth, globeWidth/2), -i, BLACK);
+                p_obs_3 = Event(ScreenToWorldX(MARGIN, globeWidth/2), i, BLACK);
+                p_obs_4 = Event(ScreenToWorldX(MARGIN + globeWidth, globeWidth/2), i, BLACK);
 
-                p1Prime = LorentzTransform(p1, velocity);
-                p2Prime = LorentzTransform(p2, velocity);
-                p3Prime = LorentzTransform(p3, velocity);
-                p4Prime = LorentzTransform(p4, velocity);
+                // Horizontal lines of the LAB's frame viewed on the OBSERVER's frame
+                p_lab_1 = LorentzTransform(p_obs_1, observerVelocity);
+                p_lab_2 = LorentzTransform(p_obs_2, observerVelocity);
+                p_lab_3 = LorentzTransform(p_obs_3, observerVelocity);
+                p_lab_4 = LorentzTransform(p_obs_4, observerVelocity);
 
-                // Draw background grid (horizontal)
+                // Drawing LAB's frame horizontal lines
                 DrawLineEx(
-                    (Vector2){WorldToScreenX(p1Prime.getX(), globeWidth/2), WorldToScreenY(p1Prime.getT(), globeHeight/2)},
-                    (Vector2){WorldToScreenX(p2Prime.getX(), globeWidth/2), WorldToScreenY(p2Prime.getT(), globeHeight/2)},
+                    (Vector2){WorldToScreenX(p_lab_1.getX(), globeWidth/2), WorldToScreenY(p_lab_1.getT(), globeHeight/2)},
+                    (Vector2){WorldToScreenX(p_lab_2.getX(), globeWidth/2), WorldToScreenY(p_lab_2.getT(), globeHeight/2)},
                     SECONDARY_GRID_THICKNESS,
                     SECONDARY_GRID_COLOR
                 );
                 DrawLineEx(
-                    (Vector2){WorldToScreenX(p3Prime.getX(), globeWidth/2), WorldToScreenY(p3Prime.getT(), globeHeight/2)},
-                    (Vector2){WorldToScreenX(p4Prime.getX(), globeWidth/2), WorldToScreenY(p4Prime.getT(), globeHeight/2)},
+                    (Vector2){WorldToScreenX(p_lab_3.getX(), globeWidth/2), WorldToScreenY(p_lab_3.getT(), globeHeight/2)},
+                    (Vector2){WorldToScreenX(p_lab_4.getX(), globeWidth/2), WorldToScreenY(p_lab_4.getT(), globeHeight/2)},
                     SECONDARY_GRID_THICKNESS,
                     SECONDARY_GRID_COLOR
                 );
 
 
-                // Draw main grid (horizontal)
+                // Drawing OBSERVER's frame horizontal lines
                 DrawLineEx(
-                    (Vector2){WorldToScreenX(p1.getX(), globeWidth/2), WorldToScreenY(p1.getT(), globeHeight/2)},
-                    (Vector2){WorldToScreenX(p2.getX(), globeWidth/2), WorldToScreenY(p2.getT(), globeHeight/2)},
+                    (Vector2){WorldToScreenX(p_obs_1.getX(), globeWidth/2), WorldToScreenY(p_obs_1.getT(), globeHeight/2)},
+                    (Vector2){WorldToScreenX(p_obs_2.getX(), globeWidth/2), WorldToScreenY(p_obs_2.getT(), globeHeight/2)},
                     GRID_THICKNESS,
                     GRID_COLOR
                 );
                 DrawLineEx(
-                    (Vector2){WorldToScreenX(p3.getX(), globeWidth/2), WorldToScreenY(p3.getT(), globeHeight/2)},
-                    (Vector2){WorldToScreenX(p4.getX(), globeWidth/2), WorldToScreenY(p4.getT(), globeHeight/2)},
+                    (Vector2){WorldToScreenX(p_obs_3.getX(), globeWidth/2), WorldToScreenY(p_obs_3.getT(), globeHeight/2)},
+                    (Vector2){WorldToScreenX(p_obs_4.getX(), globeWidth/2), WorldToScreenY(p_obs_4.getT(), globeHeight/2)},
                     GRID_THICKNESS,
                     GRID_COLOR
                 );
@@ -278,35 +320,23 @@ int main()
             currentX = globeWidth + 2*MARGIN + 30;
             currentY = MARGIN + 60;
             // GuiLabel((Rectangle){globeWidth + 2*MARGIN + 30, MARGIN + 40, panelWidth - 30, 20}, "Velocity slider");
-            GuiSlider((Rectangle){currentX, currentY, panelWidth - 200, 20}, "-C", "C", &velocity, -1.0*C, 1.0*C);
+            GuiSlider((Rectangle){currentX, currentY, panelWidth - 200, 20}, "-C", "C", &observerVelocity, -1.0*C, 1.0*C);
             currentX += panelWidth - 150;
-            DrawText(TextFormat("v = %.2f c", velocity), currentX, currentY, 20, TEXT_COLOR);
+            DrawText(TextFormat("v = %.2f c", observerVelocity), currentX, currentY, 20, TEXT_COLOR);
 
-            // Add event
+            // Event adding buttons
+            addingMode = false;
+
             currentX = globeWidth + 2*MARGIN + 30;
             currentY += 50;
-            if (GuiButton((Rectangle){currentX, currentY, 60, 30}, ""))
-            {
-                eventsList.push_back(Event(0.0, 0.0, RED));
-            }
-            DrawRectangleV((Vector2){currentX, currentY}, (Vector2){60, 30}, RED);
-            DrawRectangleLinesEx((Rectangle){currentX, currentY, 60, 30}, 2.0, BLACK);
 
-            currentX += 60 + 20;
-            if (GuiButton((Rectangle){currentX, currentY, 60, 30}, ""))
+            GuiToggleGroup((Rectangle){currentX, currentY, 60, 30}, "RED;BLUE;GREEN", &activeToggle);
+            if (activeToggle > -1)
             {
-                eventsList.push_back(Event(1.0, 1.0, BLUE));
+                addingMode = true;
+                colorIndex = activeToggle;
             }
-            DrawRectangleV((Vector2){currentX, currentY}, (Vector2){60, 30}, BLUE);
-            DrawRectangleLinesEx((Rectangle){currentX, currentY, 60, 30}, 2.0, BLACK);
 
-            currentX += 60 + 20;
-            if (GuiButton((Rectangle){currentX, currentY, 60, 30}, ""))
-            {
-                eventsList.push_back(Event(-1.0, -1.0, GREEN));
-            }
-            DrawRectangleV((Vector2){currentX, currentY}, (Vector2){60, 30}, GREEN);
-            DrawRectangleLinesEx((Rectangle){currentX, currentY, 60, 30}, 2.0, BLACK);
 
             // Clear button
             currentX = globeWidth + 2*MARGIN + 30 + panelWidth - 120;
@@ -316,10 +346,33 @@ int main()
                 eventsList.clear();
             }
             
+
+            if (addingMode)
+            {
+                mousePosition = GetMousePosition();
+                if (MouseInGrid(mousePosition, globeWidth, globeHeight))
+                {
+                    DrawCircleV(GetMousePosition(), EVENT_RADIUS, eventColorList[colorIndex][1]);
+                    if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
+                    {
+                        Event e = LorentzTransform(Event(ScreenToWorldX(mousePosition.x, globeWidth/2), ScreenToWorldY(mousePosition.y, globeHeight/2), BLACK), -observerVelocity);
+                        
+                        eventsList.push_back(
+                            Event(
+                                e.getX(),
+                                e.getT(),
+                                eventColorList[colorIndex][0]
+                            )
+                        );
+                        activeToggle = -1;
+                    }
+                }
+            }
+            
             // Draw events
             for (Event e: eventsList)
             {
-                Event ePrime = LorentzTransform(e, velocity);
+                Event ePrime = LorentzTransform(e, observerVelocity);
                 DrawCircleV(
                     (Vector2) {WorldToScreenX(ePrime.getX(), globeWidth/2), WorldToScreenY(ePrime.getT(), globeHeight/2)},
                     EVENT_RADIUS,
@@ -335,11 +388,11 @@ int main()
 }
 
 
-Event LorentzTransform(Event point, float velocity)
+Event LorentzTransform(Event point, float observerVelocity)
 {
-    float gamma = 1 / (sqrt(1 - pow(velocity/C, 2)));
-    float x_prime = gamma * (point.getX() - velocity * point.getT());
-    float t_prime = gamma * (point.getT() - (velocity * point.getX() / pow(C, 2)));
+    float gamma = 1 / (sqrt(1.0f - pow(observerVelocity/C, 2)));
+    float x_prime = gamma * (point.getX() - observerVelocity * point.getT());
+    float t_prime = gamma * (point.getT() - (observerVelocity * point.getX() / pow(C, 2)));
     
     return Event(x_prime, t_prime, point.getColor());
 }
@@ -362,4 +415,13 @@ float ScreenToWorldX(float screenX, int offsetX)
 float ScreenToWorldY(float screenY, int offsetY)
 {
     return (-screenY + MARGIN + offsetY) / GRID_SPACING ;
+}
+
+bool MouseInGrid(Vector2 mouse, float gridWidth, float gridHeight)
+{
+    if ((MARGIN <= mouse.x && mouse.x <= MARGIN + gridWidth) && (MARGIN <= mouse.y && mouse.y <= MARGIN + gridHeight))
+    {
+        return true;
+    }
+    return false;
 }
